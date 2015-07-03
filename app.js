191a44734b9fd84177a98f031707f126550fdc60
aid666 <aid666@gmail.com>
2015-07-04 00:26:43 +0800

    add JQuery

--- app.js
+++ app.js
@@ -5,25 +5,38 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var stylus = require('stylus');
+var nib = require('nib');
+
+var dashboard = require('./routes/index');
+var repo = require('./routes/repo');
 
 var app = express();
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', dashboard);
+app.use('/repo', repo);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
