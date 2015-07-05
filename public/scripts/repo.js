8421dc4521d9991b38a9bb590769d6e5f6096afd
aid666 <aid666@gmail.com>
2015-07-06 00:57:11 +0800

    Init push repo UI files

--- public/scripts/repo.js
+++ public/scripts/repo.js
@@ -2,30 +2,33 @@
  *
  */
 
-var searchDMSvc = function(searchValue)
-{
-	var dataHolder = $('#repoTable');
-	searchSvcMetadata({access:"private"}, function( json ) {
-		dataHolder.empty();
-			$.each(json, function(i, item)
-		{
-				var svcKey = item.key;
-				var row = $('<tr>');
-				row.append(
-							$('<td>').attr('width', "100%").append(
-								$('<a>').text(item.name).attr("target", "_blank").attr("href", "svc/DMService.html#"+svcKey)));
-					row.append(
-									$('<td>').append(
-										$('<a>').text("Test").attr("target", "_blank").attr("href", "Tester.html#"+svcKey)));
-					dataHolder.append(row);
-		});
-		});
-}
 
-$('#searchBtn').button().
-	text("Search").
-	click(function() {
-		searchDMSvc($('#searchField').val());
+$(document).ready(function() {
+	function searchDMSvc(searchValue)
+	{
+		var dataHolder = $('#repoTable');
+		searchSvcMetadata({access:"private"}, function( json ) {
+			dataHolder.empty();
+				$.each(json, function(i, item)
+			{
+					var svcKey = item.key;
+					var row = $('<tr>');
+					row.append(
+								$('<td>').attr('width', "100%").append(
+									$('<a>').text(item.name).attr("target", "_blank").attr("href", "svc/DMService.html#"+svcKey)));
+						row.append(
+										$('<td>').append(
+											$('<a>').text("Test").attr("target", "_blank").attr("href", "Tester.html#"+svcKey)));
+						dataHolder.append(row);
+			});
+			});
+	}
+	
+	$('#searchBtn').button().
+		text("Search").
+		click(function() {
+			searchDMSvc($('#searchField').val());
+	});
 });
 
 jQuery.fn.extend({
--- routes/repo.js
+++ routes/repo.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET public repo searcher */
 router.get('/', function(req, res, next) {
   res.render('repo', { title: 'Repository' });
+});
+/* GET users listing. */
+router.get('/svcs', function(req, res, next) {
+  res.render('repo_svcs', { title: 'My Services' });
+});
+/* GET users listing. */
+router.get('/models', function(req, res, next) {
+  res.render('repo_models', { title: 'My Models' });
+});
+/* GET users listing. */
+router.get('/trainers', function(req, res, next) {
+  res.render('repo_trainers', { title: 'My Trainers' });
 });
 
 module.exports = router;
