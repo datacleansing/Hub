3594a6bc2b762d0c05ca5ab6c01756c9e7f93d5a
aid666 <aid666@gmail.com>
2016-04-27 06:54:39 +0800

    add login

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var bodyParser = require('body-parser');
 
 var stylus = require('stylus');
@@ -11,7 +12,6 @@ var nib = require('nib');
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
 var GitHubStrategy = require('passport-github2').Strategy;
-var LocalStrategy = require('passport-local').Strategy;
 
 var index = require('./routes/index');
 var repo_jobs = require('./routes/repo_jobs');
@@ -33,6 +33,56 @@ dmcloud.repo.baseUrl = process.env.DMCLOUD_REPO_URL ? rocess.env.DMCLOUD_REPO_UR
 dmcloud.repo.modelUrl = dmcloud.repo.baseUrl + "models/"
 dmcloud.repo.archiveUrl = dmcloud.repo.baseUrl + "archives"
 
+
+function handleToken(token, tokenSecret, profile, done, domain) {
+  var account = {
+    "domain": domain,
+    "uid": profile.id,
+    "displayName": profile.displayName || profile.username,
+    "tokens": [
+      {
+        "kind": 'oauth',
+        "token": token,
+        "attributes": {
+          "tokenSecret": tokenSecret
+        }
+      }]
+  };
+  return done(null, account);
+};
+
+function handleGoogleToken(token, tokenSecret, profile, done){
+  return handleToken(token, tokenSecret, profile, done, "google.com");
+}
+
+function handleGitHubToken(token, tokenSecret, profile, done){
+  return handleToken(token, tokenSecret, profile, done, "github.com");
+}
+
+passport.use(new GoogleStrategy({
+    consumerKey: process.env.GOOGLE_CONSUMER_KEY || "",
+    consumerSecret: process.env.GOOGLE_CONSUMER_SECRET || "",
+    callbackURL: "/auth/google/callback"
+  },
+  handleGoogleToken
+));
+
+passport.use(new GitHubStrategy({
+    "clientID": process.env.GITHUB_CLIENT_ID || "",
+    "clientSecret": process.env.GITHUB_CLIENT_SECRET || "",
+    "callbackURL": "/auth/github/callback"
+  },
+  handleGitHubToken
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -43,122 +93,48 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Initialize Passport!  Also use passport.session() middleware, to support
-// persistent login sessions (recommended).
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+  secret: '12345',
+  name: 'datacleansing_logon',
+  cookie: {maxAge: 30*60*1000 },
+  resave: false,
+  saveUninitialized: true,
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    console.log("au");
-    console.log(username);
-    console.log(password);
-    // User.findOne({ username: username }, function(err, user) {
-    //   if (err) { return done(err); }
-    //   if (!user) {
-    //     return done(null, false, { message: 'Incorrect username.' });
-    //   }
-    //   if (!user.validPassword(password)) {
-    //     return done(null, false, { message: 'Incorrect password.' });
-    //   }
-    //   return done(null, user);
-    // });
+function handleAuthCallback(req, res){
+    res.redirect('/ui/');
+};
 
-    return done(null, {})
-  }
-));
-
-app.post('/login', passport.authenticate('local',
-  { successRedirect: '/ui/', failureRedirect: '/login' })
-);
-
-GOOGLE_CONSUMER_KEY = process.env.GOOGLE_CONSUMER_KEY || "";
-GOOGLE_CONSUMER_SECRET = process.env.GOOGLE_CONSUMER_SECRET || "";
-passport.use(new GoogleStrategy({
-    consumerKey: GOOGLE_CONSUMER_KEY,
-    consumerSecret: GOOGLE_CONSUMER_SECRET,
-    callbackURL: "/auth/google/callback"
-  },
-  function(token, tokenSecret, profile, done) {
-    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
-    //   return done(err, user);
-    // });
-
-  }
-));
-// GET /auth/google
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in Google authentication will involve redirecting
-//   the user to google.com.  After authorization, Google will redirect the user
-//   back to this application at /auth/google/callback
-app.get('/auth/google',
-  passport.authenticate('google', { scope: 'http://hub.datacleansing.io/' })
-);
-
-// GET /auth/google/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
+app.get('/auth/google', passport.authenticate('google', { scope: 'https://www.google.com/m8/feeds' }));
 app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
+  passport.authenticate('google'),
+  handleAuthCallback);
 
-GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || "";
-GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET || "";
-passport.use(new GitHubStrategy({
-    "clientID": GITHUB_CLIENT_ID,
-    "clientSecret": GITHUB_CLIENT_SECRET,
-    "callbackURL": "/auth/github/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    // User.findOrCreate({ githubId: profile.id }, function (err, user) {
-    //   return done(err, user);
-    // });
-  }
-));
-
-app.get('/auth/github',
-  passport.authenticate('github'),
-  function(req, res){
-    // The request will be redirected to Github for authentication, so this
-    // function will not be called.
-  });
-
-// GET /auth/weibo/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
+app.get('/auth/github', passport.authenticate('github', { failureRedirect: '/login', successRedirect: '/ui/'}));
 app.get('/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
+  passport.authenticate('github'),
+  handleAuthCallback);
 
 app.get('/logout', function(req, res){
   req.logout();
-  res.redirect('/');
+  res.redirect('/login');
 });
 
-app.use(function(req, res, next) {
-
-  //to allow cross domain requests to send cookie information.
-  //res.header('Access-Control-Allow-Credentials', true);
-
-  // origin can not be '*' when crendentials are enabled. so need to set it to the request origin
-  res.header('Access-Control-Allow-Origin',  '*');
-
-  // list of methods that are supported by the server
-  res.header('Access-Control-Allow-Methods','OPTIONS,GET,PUT,POST,DELETE');
-
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-
-  next();
-});
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
 
 app.use(stylus.middleware(
   { src: __dirname + '/public'
@@ -168,6 +144,26 @@ app.use(stylus.middleware(
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+
+app.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+app.use(function(req, res, next) {
+  if(req.isAuthenticated()){
+    res.header('Access-Control-Allow-Origin',  '*');
+    res.header('Access-Control-Allow-Methods','OPTIONS,GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+  }else{
+    res.render("403");
+  }
+});
+app.get('/ui/', function(req, res, next) {
+res.render('dashboard', {
+  "user": req.user
+});
+});
 app.use('/ui/jobs', repo_jobs);
 app.use('/ui/models', repo_models);
 app.use('/ui/archives', repo_archives);
